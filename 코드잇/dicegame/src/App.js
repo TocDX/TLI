5eba8e495f6a52ac6e9073eb0daee3c3208550a9
TocDX <sk372021000@gmail.com>
2023-05-29 18:48:38 +0900

    react

--- 코드잇/dicegame/src/App.js
+++ 코드잇/dicegame/src/App.js
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import Dice from'./dice';
+import Button from './Button';
 
+function random(n){
+    return Math.ceil(Math.random() * n);
+}
 
 function App(){
+    const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
+
+    const handleRollClick = () => {
+        const nextNum = random(6);
+        setNum(nextNum);
+        setSum(sum + nextNum);
+        // gameHistory.push(nextNum);
+        setGameHistory([...gameHistory, nextNum]);
+    };
+
+    const handleClearClick = () => {
+        setNum(1);
+        setSum(0);
+        setGameHistory([]);
+    }
+
+    
     return (
         <div>
-            <Dice/>
+            <div>
+                <Button onClick={handleRollClick}>던지기</Button>
+                <Button onClick={handleClearClick}>처음부터</Button>
+            </div>
+            <h2>나</h2>
+            <Dice color="red" num={num}/>
+            <h2>총점</h2>
+            <p>{sum}</p>
+            <h2>기록</h2>
+            <p>{gameHistory.join(', ')}</p>
         </div>
     );
 }
