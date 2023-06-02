15233e8a4aabcfa06b1b92b7bcc031ceafa18dc9
TocDX <sk372021000@gmail.com>
2023-06-02 22:20:59 +0900

    react

--- 코드잇/moviepedia/src/component/Fileinput.js
+++ 코드잇/moviepedia/src/component/Fileinput.js
@@ -1,9 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
+  const inputRef = useRef();
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
-  return <input type="file" onChange={handleChange}></input>;
+
+  useEffect(() => {
+    if (!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+  }, [value]);
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+    inputNode.value = "";
+    onChange(name, null);
+  };
+  return (
+    <div>
+      <img src={preview} alt="이미지 미리보기"></img>
+      <input type="file" onChange={handleChange} ref={inputRef}></input>
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
 
 export default FileInput;
